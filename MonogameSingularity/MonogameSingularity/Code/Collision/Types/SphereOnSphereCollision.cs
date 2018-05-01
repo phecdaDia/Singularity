730109108a16ac628359cabcdac81fbbf1d33bac
imthe666st <imthe666st@gmail.com>
2018-05-01 09:15:51 +0200

    Added SphereCollisions (WIP #45)

---
+++ MonogameSingularity/MonogameSingularity/Code/Collision/Types/SphereOnSphereCollision.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singularity.Code.Collision.Types
+{
+	public class SphereOnSphereCollision
+	{
+
+		public static Boolean DoesCollide(SphereCollision colliderA, SphereCollision colliderB)
+		{
+			// spheres collide if the distance between them is <= their radius added
+			return (colliderA.Position - colliderB.Position).Length() <= colliderA.Radius + colliderB.Radius;
+		}
+
+	}
+}
