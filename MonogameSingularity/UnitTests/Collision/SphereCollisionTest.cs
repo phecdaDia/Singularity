b5581a79dd4ecbc1c47b01e30233078b78205362
imthe666st <imthe666st@gmail.com>
2018-05-01 09:39:27 +0200

    Tried to setup the Unit Test again, however the error is still there.

---
+++ MonogameSingularity/UnitTests/Collision/SphereCollisionTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xna.Framework;
+using Singularity.Code.Collision;
+
+namespace UnitTests.Collision
+{
+	[TestClass]
+	public class SphereCollisionTest
+	{
+		[TestMethod]
+		public void SphereOnSphereTest()
+		{
+			//// create 3 spheres
+			//SphereCollision sc1 = new SphereCollision(new Vector3(1, 1, 1), 1.0f);
+
+			//// check if Vector3 is working
+			//Assert.IsTrue(sc1.Position.X == 1.0f);
+			
+		}
+	}
+}
