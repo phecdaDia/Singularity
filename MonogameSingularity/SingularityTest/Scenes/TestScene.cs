3152ebb036c0e16fb8309497ae834ae4a7fbb201
imthe666st <imthe666st@gmail.com>
2018-06-11 20:13:38 +0200

    Added rotating sphere in test scene again and tested ray on sphere collision

--- MonogameSingularity/SingularityTest/Scenes/TestScene.cs
+++ MonogameSingularity/SingularityTest/Scenes/TestScene.cs
@@ -146,6 +146,10 @@ namespace SingularityTest.Scenes
 
 			}));
 
+			AddObject(new EmptyGameObject().SetPosition(0, 10, 0).AddScript(
+				((scene, o, arg3) => o.AddRotation(0, (float)arg3.ElapsedGameTime.TotalSeconds, 0))
+			).AddChild(new CollidableModelObject("sphere").SetPosition(5, 0, 0)));
+
 		}
 
 		public override void AddLightningToEffect(BasicEffect effect)
