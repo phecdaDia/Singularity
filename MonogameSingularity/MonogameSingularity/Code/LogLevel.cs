adb9e020ae2f31e6f08ea26a4c69565e8cd81f71
imthe666st <imthe666st@gmail.com>
2018-09-12 05:03:17 +0200

    Added basic logger (#134)

---
+++ MonogameSingularity/MonogameSingularity/Code/LogLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singularity
+{
+	public enum LogLevel
+	{
+		Information = 0,
+		Warning = 1,
+		Error = 2,
+	}
+}
